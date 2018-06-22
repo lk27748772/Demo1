84fd795d038a13f34ddfa57e1c1fc6c0e77d09d5
liukun <lookid@sina.cn>
2018-06-22 17:38:58 +0800

    bug修改

---
+++ mylibrary/src/main/java/com/lookid/mylibrary/UIUtilsLib.java
@@ -0,0 +1,25 @@
+package com.lookid.mylibrary;
+
+import android.content.Context;
+import android.os.Handler;
+
+
+/**
+ * 系统资源相关
+ * Created by kunliu on 2017/8/14.
+ */
+public class UIUtilsLib {
+
+    public static Context getContext() {
+        return null;
+    }
+
+    public static Handler getHandler() {
+        return new Handler();
+    }
+
+    public static int getMainThreadId() {
+        return 0;
+    }
+
+}
