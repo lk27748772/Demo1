ebd9a4c23b289ef89e919bbfc70680e56d66a361
liukun <lookid@sina.cn>
2018-06-22 18:32:43 +0800

    代码修改

--- app/src/main/java/com/lookid/demo1/MainActivity1.java
+++ app/src/main/java/com/lookid/demo1/MainActivity1.java
@@ -3,7 +3,7 @@ package com.lookid.demo1;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
